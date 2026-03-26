fff0e759f899bbdc65465861431e625df1845526
202310055HJ <152050828+202310055HJ@users.noreply.github.com>
2026-03-26 14:35:19 +0900

    Add files via upload

---
+++ 0326-3.py
@@ -0,0 +1,24 @@
+for i in range(20):
+    print(i + 1)
+
+for n in range(1, 10 + 1):
+    print(n)
+
+total = 0
+for n in range(1, 10 + 1):
+    total += n
+    print(total)
+
+dan = 5
+for n in range(1, 10 + 1):
+    print(f"{dan} x {n} = {n * dan}")
+
+total = 0
+for n in range(1, 250 + 1):
+    if n % 2 == 0:
+        total += n
+        print(total)
+
+
+
+
---
+++ 0326.py
@@ -0,0 +1,16 @@
+numbers = [10, 90,20,30,40]
+numbers.append(100)
+numbers
+print(numbers)
+numbers.extend([101,102,103])
+print(numbers)
+print(len(numbers))
+print(sum(numbers))
+print(max(numbers))
+print(min(numbers))
+numbers.sort()
+print(numbers)
+print(numbers[0])
+print(numbers[-1])
+print(numbers[len(numbers)//2])
+
